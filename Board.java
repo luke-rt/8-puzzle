708edde7262d6cb4248a45517d96235f75edca12
Zelo-S <45673317+Zelo-S@users.noreply.github.com>
2023-03-08 21:22:19 -0500

    Correct Hamming

--- Board.java
+++ Board.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-import javax.naming.ldap.ManageReferralControl;
-
 public class Board {
     private int[][] board;
     private int moves;
@@ -15,15 +13,17 @@ public class Board {
         } 
     }           // construct a board from an N-by-N array of blocks
                                            // (where blocks[i][j] = block in row i, column j)
+
     public int size() {
         return board.length;
     }                      // board size N
+
     public int hamming() {
         // number of blocks out of place
         int incorrect = 0;
         for(int i = 1; i <= this.size(); ++i) {
             for(int j = 1; j <= this.size(); ++j) {
-                if(board[i][j] != i*j) ++incorrect;
+                if(board[i][j] != i+j) ++incorrect;
             }
         }
         return incorrect + this.moves;
@@ -36,34 +36,60 @@ public class Board {
                 if(board[row][col] != 0){
                     int proper_row = board[row][col] / this.size();
                     int proper_col = (board[row][col] % this.size()) - 1;
-                    
                     if(board[row][col] == this.size()){
                         proper_row--;
                         proper_col += this.size();
                     }
-                    System.out.println("Moving " + board[row][col] + " by " + (Math.abs(row - proper_row) + Math.abs(col - proper_col)));
-                    
                     manhat_dist += Math.abs(row - proper_row) + Math.abs(col - proper_col);
                 }
             }
         } 
-        System.out.println("Dist: " + manhat_dist);
         return manhat_dist;
     }                 // sum of Manhattan distances between blocks and goal
+
     public boolean isGoal() {
-        return false;
+        return manhattan() == 0; // if manhattan is 0, then board is proper goal
     }                // is this board the goal board?
+
     public boolean isSolvable(){
+        int inversions = 0;
+        if(this.size() % 2 == 1){
+            for(int row1=0; row1<this.size(); ++row1){
+                for(int col1=0; col1<this.size(); ++col1){
+                    for(int row2=0; row2<this.size(); ++row2){
+                        for(int col2=0; col2<this.size(); ++col2){
+                            if(board[row1][col1] < board[row2][col2] && (row1 > row2 || col1 > col2)){
+                                ++inversions;
+                            }
+                        }
+                    }
+                }
+            }
+            
+            if(inversions % 2 == 1) return false;
+        }else if(this.size() % 2 == 0){
+
+        }
         return false;
     }            // is this board solvable?
+
     public boolean equals(Object y) {
-        return false;
+        return y.equals(board);
     }        // does this board equal y?
+
     public Iterable<Board> neighbors() {
         return null;
     }     // all neighboring boards
+
     public String toString() {
-        return null;
+        String board_string = "";
+        for(int i=0; i<this.size(); ++i){
+            for(int e=0; e<this.size(); ++e){
+                board_string += board[i][e] + "  ";
+            }
+            board_string += "\n";
+        }
+        return board_string;
     }               // string representation of this board (in the output format specified below)
 
     public static void main(String[] args) {
@@ -87,5 +113,6 @@ public class Board {
         }
     
         System.out.println(initial.manhattan());
+        System.out.println(initial);
     }
 }
