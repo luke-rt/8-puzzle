0ee0cc91c80a3db64b2cdb966866099ff9a973f0
Zelo-S <45673317+Zelo-S@users.noreply.github.com>
2023-03-11 23:09:44 -0500

    Started Solver

--- Solver.java
+++ Solver.java
@@ -1,13 +1,29 @@
 public class Solver {
     private MinPQ<Board> pq;
+    private Board workingBoard;
     public Solver(Board initial){
-
+        pq = new MinPQ<Board>();
+        workingBoard = initial;
     }           // find a solution to the initial board (using the A* algorithm)
     public int moves(){
 
     }                     // min number of moves to solve initial board
-    public Iterable<Board> solution(){
+    
+    public void helper(Board b) {
+        pq.insert(b);
+        for(Board b1 : b.neighbors()){
+            for(Board b2 : b1.neighbors()) {
+                if(!b2.equals(b)){
 
+                }
+            }
+        }
+    }
+
+    public Iterable<Board> solution(){
+        while(!workingBoard.isGoal()){
+            helper(workingBoard);
+        }
     }      // sequence of boards in a shortest solution
     public static void main(String[] args){
 
