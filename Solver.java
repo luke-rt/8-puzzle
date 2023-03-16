7a2aeeaf4d0641c5248500356aba98e074c9e547
ZeloS <stevethesteve990@gmail.com>
2023-03-16 14:26:31 -0400

    Custom node implemented

--- Solver.java
+++ Solver.java
@@ -1,32 +1,71 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
+
+class Node implements Comparable<Node>{
+    private Board b;
+    private int moves;
+    public Node(Board b, int moves){
+        this.b = b;
+        this.moves = moves;
+    }
+    
+    public int getMoves(){
+        return this.moves;
+    }
+    public Board getBoard(){
+        return this.b;
+    }
+    public int getPriority(){
+        return this.moves + this.b.manhattan();
+    }
+    public int compareTo(Node a){
+        return Integer.compare(this.getPriority(), a.getPriority());
+    }
+}
+
 public class Solver {
-    private MinPQ<Board> pq;
-    private Board workingBoard;
+    
+    
+    private MinPQ<Node> pq;
+    private Node initNode;
     public Solver(Board initial){
-        pq = new MinPQ<Board>();
-        workingBoard = initial;
+        pq = new MinPQ<Node>();
+        initNode = new Node(initial, 0);
     }           // find a solution to the initial board (using the A* algorithm)
+
     public int moves(){
+        
+        return 0;
 
     }                     // min number of moves to solve initial board
     
-    public void helper(Board b) {
-        pq.insert(b);
-        for(Board b1 : b.neighbors()){
-            for(Board b2 : b1.neighbors()) {
-                if(!b2.equals(b)){
-                    
-
-                }
-            }
+    public void helper(Node node) {
+        pq.insert(node);
+        for(Board b : node.getBoard().neighbors()){
+            pq.insert(new Node(b, node.getMoves()+1));
         }
+        
+        Node min = pq.delMin();
+        System.out.println(min.getPriority());
     }
 
     public Iterable<Board> solution(){
-        while(!workingBoard.isGoal()){
-            helper(workingBoard);
-        }
+        ArrayList<Board> empty = new ArrayList<>();
+        helper(initNode);
+        return empty;
     }      // sequence of boards in a shortest solution
-    public static void main(String[] args){
 
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int[][] blocks = new int[N][N];
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                blocks[i][j] = in.nextInt();
+        Board initial = new Board(blocks);
+        Solver s = new Solver(initial);
+        
+        s.solution();
     } // solve a slider puzzle (given below) 
 }
