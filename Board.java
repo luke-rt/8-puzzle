eff6db71517adec707f58329caab27d038d23f3f
ZeloS <stevethesteve990@gmail.com>
2023-03-23 18:33:26 -0400

    updated file headers

--- Board.java
+++ Board.java
@@ -1,3 +1,12 @@
+/*
+ * Steve X + Luke T
+ * 3.23.23
+ * Java 8
+ * 
+ * 
+ * 
+ * The problem. The 8-puzzle problemLinks to an external site. is a puzzle invented and popularized by Noyes Palmer Chapman in the 1870s. It is played on a 3-by-3 grid with 8 square blocks labeled 1 through 8 and a blank square. Your goal is to rearrange the blocks so that they are in order, using as few moves as possible. You are permitted to slide blocks horizontally or vertically into the blank square. The following shows a sequence of legal moves from an initial board (left) to the goal board (right).
+ */
 import java.util.ArrayList;
 import java.util.Scanner;
 
